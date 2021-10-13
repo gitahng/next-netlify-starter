ffb7e2ea9432500e5f7ea73ecd3ea48c72f6c8fa
gitahng <92431327+gitahng@users.noreply.github.com>
2021-10-13 16:38:46 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
-    das ist meine neue seite
+    das ist meine neue seite</code>
         </p>
       </main>
 
